0c04d16b23955dda609bf7b79754c5133c9d3cd5
Joko-eng <mohammadjoko1001@gmail.com>
2024-11-12 08:17:40 +0700

    terbaru

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -98,7 +98,6 @@
                                             <div class="col">
                                                 <div class="progress progress-sm mr-2">
                                                     <div class="progress-bar bg-success" role="progressbar"
-                                                        style="width: {{ $persentaseTerima }}%"
                                                         aria-valuenow="{{ $persentaseTerima }}" aria-valuemin="0"
                                                         aria-valuemax="100">
                                                     </div>
@@ -135,7 +134,7 @@
                                             <div class="col">
                                                 <div class="progress progress-sm mr-2">
                                                     <div class="progress-bar bg-warning" role="progressbar"
-                                                        style="width: {{ $persentaseTolak }}%"
+                                                        
                                                         aria-valuenow="{{ $persentaseTolak }}" aria-valuemin="0"
                                                         aria-valuemax="1000">
                                                     </div>
