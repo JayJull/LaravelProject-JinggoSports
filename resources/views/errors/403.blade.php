b656ffa2d1c4727879d6a3fb813fbbcbf4ea96f7
Joko-eng <mohammadjoko1001@gmail.com>
2024-12-17 11:59:18 +0700

    feature: view eror 404

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <h1>Anda harus login sesuai role yang dibutuhkan</h1>  
+</body>
+</html>
