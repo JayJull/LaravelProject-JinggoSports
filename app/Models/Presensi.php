c9043e3f57d80ffe25a4a078c9205838ca70babf
cay <cronosstar007@gmail.com>
2024-12-18 12:26:14 +0700

    update presensi

--- app/Models/Presensi.php
+++ app/Models/Presensi.php
@@ -258,7 +258,7 @@ class Presensi extends Model
         return redirect()->route('aktif-presensi')->with('success', 'berhasil aktifasi');
     }
 
-    public static function Scan($request){//ubah ini
+    public static function Scan($request){//ubah ini tamnbai
         // dd($request);
         $userLogin = Auth::user();
 
